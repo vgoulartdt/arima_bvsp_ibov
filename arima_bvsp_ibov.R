cdf6c1c71018bb789d20f14818c72a3cfd0fbfb7
Victor Goulart <122612734+vgoulartdt@users.noreply.github.com>
2023-12-03 14:38:41 -0300

    Update arima_bvsp_ibov.R

--- arima_bvsp_ibov.R
+++ arima_bvsp_ibov.R
@@ -2,83 +2,89 @@ library(tseries)
 library(quantmod)
 library(forecast)
 
-date <- as.Date("2023-11-22")
+#Set the date
+date <- as.Date("2023-11-15")
 
+#Get historical data of the time serie
 ?getSymbols
 getSymbols("^BVSP", src = "yahoo", to = date)
 
+#Extracting closing prices
 asset <- BVSP$BVSP.Close
 head(BVSP$BVSP.Close)
 
+#Check for missing values
 any(is.na(asset))
 asset <- na.omit(asset)
 any(is.na(asset))
 
-#decomposing the time serie
+str(asset)
+
+#Decomposing the time serie
 asset.ts <- ts(asset, frequency = 252)
 plot(decompose(asset.ts))
 plot(decompose(asset.ts, type = "additive", filter = NULL))
 plot(decompose(asset.ts, type = "multiplicative", filter = NULL))
 
-#checking for stationarity at the chart
-str(asset)
+#Check for stationarity at the chart
 plot.ts(asset)
 
 #Augmented Dickey-Fuller Test (ADF) for stationarity
-#H0 -> non-stationary, H1 -> stationary
+#H0 -> non-stationary, H1 -> stationary (alternative hyphothesis)
 adf.test(asset)
-#We do not reject H0 (null hypothesis) for 5% of significance, so the data isn't stationary (p-value > 0.05)
+#We do not reject H0 (null hypothesis) at a 5% of significance, so the data isn't stationary (p-value > 0.05)
 
 #Phillips Perron Test (PP) for stationarity
-##H0 -> non-stationary, H1 -> stationary
+#H0 -> non-stationary, H1 -> stationary
 pp.test(asset)
-#We do not reject H0 for 5% of significance, so the data isn't stationary (p-value > 0.05)
+#We do not reject H0 at a 5% of significance, so the data isn't stationary (p-value > 0.05)
 
-#Transforming the data for stationarity taking the log-returns
+#Transform the data for stationarity by taking the log-returns
 d.asset <- diff(log(asset))
 
-#plotting the log-returns
+#Plotting the log-returns
 plot.ts(d.asset)
 
-#omitting NA values
+#Omitting NA values
 any(is.na(d.asset))
 d.asset <- na.omit(d.asset)
 any(is.na(asset))
 
-#testing again for stationarity
+#Test for stationarity again
 adf.test(d.asset)
 pp.test(d.asset)
 
+plot.ts(d.asset)
 
 
 
 #################################################################
 ########################## ARIMA
 
-#Veryfing the autocorrelations of the time serie
+#Check autocorrelations of the time serie
 acf(d.asset)
 pacf(d.asset)
 
-#Auto.arima function checks the best ARIMA specifications to fit the model (p,d,q)
+#Auto.arima function checks automatically the best ARIMA specifications (p,d,q) to fit the model
 auto.arima(d.asset, trace = T)
 auto.arima(d.asset, trace = T, stepwise = F, approximation = F)
 ?auto.arima
 
-#Now we're able to check the estimations for the paramaters of the model
-#although the auto.arima suggested (1,0,0) or (4,0,1), the (4,0,2) gave the lowest AIC and the hightest p-value for Ljung-box Test
+#Now we're able to check the estimated parameters (coefficients) for the model
+#although auto.arima suggested (1,0,0) or (4,0,1), (4,0,2) gave the lowest AIC and the highest p-value for Ljung-box Test
 arima_d.asset <- Arima(d.asset, order = c(4,0,2))
 arima_d.asset
 
 #### Diagnostic Checks
 
-#Checking the autocorrelation of the residuals
+#Check the autocorrelation of the residuals
 acf(arima_d.asset$residuals)
 pacf(arima_d.asset$residuals)
 
-#Plotting to check if there is zero mean on the residuals of the ARIMA model
+#Plotting to check if there is a zero mean on the residuals of the ARIMA model
 plot.ts(arima_d.asset$residuals)
 
-#Checking histogram to see if the residuals are normally distributed
+#Check histogram to see if the residuals are normally distributed
 gghistogram(arima_d.asset$residuals)
 #It is bell shaped around zero
 
@@ -95,7 +101,7 @@ plot(forecast_d.asset)
 
 ####Reverting diff-log-returns
 
-#Last observed value of the original series
+#Last observed value of the original time series
 last_observed_price <- asset[length(asset)]
 
 #List to store the predicted prices
@@ -105,7 +111,7 @@ for (log_return in forecast_d.asset$mean) {
   #Calculate the next predicted price based on the forecasted log-return
   next_predicted_price <- last_observed_price * exp(log_return)
   
-  # dd the predicted price to the list
+  #Add the predicted price to the list
   predicted_prices <- c(predicted_prices, next_predicted_price)
   
   #Update the "last observed price" for the next loop
@@ -116,24 +122,25 @@ for (log_return in forecast_d.asset$mean) {
 print(predicted_prices)
 
 
-#This will provide the six predicted prices based on the log-returns forecasted by the ARIMA model. Once you have these prices, you can compare them with any actual future data you obtain for the original time serie.
-#As always, when working with financial forecasts, it is crucial to exercise caution and not make investment decisions solely based on statistical models. Models provide valuable information, but there are always uncertainties and other factors to consider.
+#This will provide the three predicted prices based on the log-returns forecasted by the ARIMA model. Once you have these prices, you can compare them with any actual future data you obtain for the original time serie.
+
+##As always, when working with financial forecasts, it is crucial to exercise caution and not make investment decisions solely based on statistical models. Models provide valuable information, but there are always uncertainties and other factors to consider.
 
 
 
-#### Veryfing the quality of the forecast with past values
+####Verifying the quality of the forecast with past values
 
 count_values <- sum(!is.na(d.asset))
 print(count_values)
 
-#Omiting the last 6 values of our original time serie to see if the forecasting is well done for these 3 years.
-#If we have 4175 values we have to define 1:4174 to check the last value of the time serie and compare it with the forecast.
-arima_d.asset2 <- Arima(d.asset[1:4174], order = c(4,0,2))
-forecast_d.asset2 <- forecast(arima_d.asset2, h = 1)
+#Omiting the last 3 values (h) of our original time serie to see if the forecasting is reasonable
+#If we have 4173 values, we have to define [1:4170] to check the last 3 values of the time serie and compare it with the forecast.
+arima_d.asset2 <- Arima(d.asset[1:4170], order = c(4,0,2))
+forecast_d.asset2 <- forecast(arima_d.asset2, h = 3)
 forecast_d.asset2
 
-#Checking if they are good comparing with the actual last value of the serie
-tail(d.asset, n = 1)
+#Checking if they are good by comparing with the 3 last values of the original time serie
+tail(d.asset, n = 3)
 
 
 
@@ -144,20 +151,24 @@ tail(d.asset, n = 1)
 ################################## For weekly chart
 ##############################################################################
 
+#Get data until the specified date
+ibov.weekly <- to.weekly(getSymbols("^BVSP", src = "yahoo", to = date, auto.assign = FALSE))
+
 ibov.weekly <- to.weekly(BVSP$BVSP.Close)
 ibov.weekly1 <- ibov.weekly$`BVSP$BVSP.Close.Close`
 head(ibov.weekly1)
+str(ibov.weekly1)
 
 any(is.na(ibov.weekly1))
 
-#Checking stationarity with the charts
-str(ibov.weekly1)
+#Checking stationarity with the charts first
 plot.ts(ibov.weekly1)
+#Clearly not stationary
 
 #Augmented Dickey-Fuller Test (ADF) for stationarity
 #H0 -> non-stationary, H1 -> stationary
 adf.test(ibov.weekly1)
-#We do not reject H0 (null hypothesis) for 5% of significance, so the data isn't stationary (p-value > 0.05) according to ADF Test
+#We do not reject H0 (null hypothesis) at a 5% of significance, so the data isn't stationary (p-value > 0.05) according to ADF Test
 
 #Phillips Perron Test (PP) for stationarity
 ##H0 -> non-stationary, H1 -> stationary
@@ -165,10 +176,10 @@ pp.test(ibov.weekly1)
 #We do not reject H0 for 5% of significance, so the data isn't stationary (p-value > 0.05) according to PP Test
 
 
-#Transforming the data for stationarity taking the log-returns
+#Transform the data for stationarity by taking the log-returns
 dibov_weekly <- diff(log(ibov.weekly1))
 
-#plotting the log-returns
+#Plotting the log-returns
 plot.ts(dibov_weekly)
 
 any(is.na(dibov_weekly))
@@ -184,11 +195,11 @@ pp.test(dibov_weekly)
 #################################################################
 ########################## ARIMA weekly
 
-#Seeing the autocorrelations of the time serie
+#See the autocorrelations of the time serie
 acf(dibov_weekly)
 pacf(dibov_weekly)
 
-#Machine learning algoritmn that checks the best ARIMA model
+#Auto.arima
 auto.arima(dibov_weekly)
 auto.arima(dibov_weekly, trace = T, stepwise = F, approximation = F)
 
@@ -198,57 +209,57 @@ arima_dibov_weekly
 
 #### Diagnostic Checks
 
-#Checking the autocorrelation of the residuals
+#Check the autocorrelation of the residuals
 acf(residuals(arima_dibov_weekly))
 pacf(residuals(arima_dibov_weekly))
 
-#Plotting to check if there is zero mean on the residuals of the ARIMA model
+#Plotting to check if there is a zero mean on the residuals of the ARIMA model
 plot.ts(residuals(arima_dibov_weekly))
 
-#Checking histogram to see if the residuals are normally distributed
+#Check histogram to see if the residuals are normally distributed
 gghistogram(residuals(arima_dibov_weekly))
-# It is bell shaped around zero
+#It is bell shaped around zero
 
 #Ljung-box Test
 Box.test(residuals(arima_dibov_weekly), lag = 10, type = c("Box-Pierce", "Ljung-Box"))
-#H0 = Residuals are independent (iid), in this case we do not reject our H0 because p-value > 0.05
+#H0 = Residuals are independent and identically distributed (iid), in this case we do not reject our H0 because p-value > 0.05
 
 count_values_weekly <- sum(!is.na(dibov_weekly))
 print(count_values_weekly)
 
 #Forecasting
-forecast_ibov_weekly <- forecast(arima_dibov_weekly, h=6)
+forecast_ibov_weekly <- forecast(arima_dibov_weekly, h=3)
 forecast_ibov_weekly
 plot(forecast_ibov_weekly)
 
-### Veryfing the quality of the forecast
+###Veryfing the quality of the forecast
 
-#Omiting the last 6 values of our original time serie to see if the forecasting is well done for these 3 years
-arima_dibov2 <- Arima(dibov_weekly[1:879], order = c(6,0,2))
-forecast_ibov_weekly2 <- forecast(arima_dibov2, h = 1)
+#Omit the last 3 values (h) of our original time serie to see if the forecasting is reasonable
+arima_dibov2 <- Arima(dibov_weekly[1:877], order = c(6,0,2))
+forecast_ibov_weekly2 <- forecast(arima_dibov2, h = 3)
 forecast_ibov_weekly2
 
-#Checking if they are good
-tail(dibov_weekly, n = 1)
+#Check if they are good
+tail(dibov_weekly, n = 3)
 
 
 
-# Last observed value of the original series
+#Last observed value of the original series
 last_observed_price <- ibov.weekly1[length(ibov.weekly1)]
 
-# List to store the predicted prices
+#List to store the predicted prices
 predicted_prices <- numeric()
 
 for (log_return in forecast_ibov_weekly$mean) {
-  # Calculate the next predicted price based on the forecasted log-return
+  #Calculate the next predicted price based on the forecasted log-return
   next_predicted_price <- last_observed_price * exp(log_return)
   
-  # Add the predicted price to the list
+  #Add the predicted price to the list
   predicted_prices <- c(predicted_prices, next_predicted_price)
   
-  # Update the "last observed price" for the next loop
+  #Update the "last observed price" for the next loop
   last_observed_price <- next_predicted_price
 }
 
-# Print the predicted prices
+#Print the predicted prices
 print(predicted_prices)
